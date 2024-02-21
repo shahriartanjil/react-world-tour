4908d08a688e9a92224c38b17bef94a08ff20266
Muhammad Shahriar Hasan <muhammad15-13854@diu.edu.bd>
2024-02-21 13:41:30 +0600

    finished

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,20 +6,58 @@ import './countries.css';
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const[visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data));
     }, [])
+
+    const handleVisitedCountry = country =>{
+        console.log('Add this to yur visited Country')
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = flag =>{
+        // console.log('flags adding');
+        const newVisitedFlags =[...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
+
+    // remove item from an array in a state
+    // use filter to select all the elements except the one you want to remove
+    
+
     return (
         <div >
             <h3>Countries: {countries.length}</h3>
+            {/* visited countries */}
+            <div>
+                <h5>Visited countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li 
+                        key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="flag-container">
+                {
+                    visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
+                }
+            </div>
+            {/* displayed countries */}
            <div className="country-container">
            {
             countries.map(country => <Country 
-            key={country.cca3} country={country}>
-             </Country>)
+            key={country.cca3} 
+            handleVisitedCountry={handleVisitedCountry}
+            handleVisitedFlags={handleVisitedFlags}
+            country={country}></Country>)
            }
            </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+import CountryDetail from '../CountryDetails/CountryDetail';
+const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
@@ -10,18 +11,33 @@ const Country = ({country}) => {
         setVisited(!visited);
     }
 
+    // console.log(handleVisitedCountry);
+    // const passWithParams = () => handleVisitedCountry(country);
+    
+
 
     return (
-        <div className='country'>
-            <h3>{name.common}</h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color: visited ? 'purple': 'white'}}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
-            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add flag</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}
+            </button>
             {/* {visited && ' I hae visited this country'} */}
             {visited ? ' I have visited this country' : 
             'i want to visit'}
+            <hr />
+            <CountryDetail>
+            country ={country}
+            handleVisitedCountry ={handleVisitedCountry}
+            handleVisitedFlags ={handleVisitedFlags}
+            </CountryDetail>
         </div>
     );
 };
